9e41862adb55fb8ea9bb095b0cf999d0331715a2
Štěpán Cellar <stepan.cellar@gmail.com>
2019-07-19 16:45:55 +0200

    User Management with OIDC - pac4j, Okta

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,14 @@
 package controllers;
 
 import play.mvc.*;
+import com.google.inject.Inject;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.play.PlayWebContext;
+import org.pac4j.play.java.Secure;
+import org.pac4j.play.store.PlaySessionStore;
+
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -20,4 +28,25 @@ public class HomeController extends Controller {
         return ok(views.html.index.render());
     }
 
+    @Secure(clients = "OidcClient")
+    public Result oidcIndex() { return protectedIndexView(); }
+
+    @Secure
+    public Result protectedIndex() {
+        return protectedIndexView();
+    }
+
+    private Result protectedIndexView() {
+        return ok(views.html.protectedIndex.render(getProfiles()));
+    }
+
+    @Inject
+    private PlaySessionStore playSessionStore;
+
+    @SuppressWarnings("unchecked")
+    private List<CommonProfile> getProfiles() {
+        final PlayWebContext context = new PlayWebContext(ctx(), playSessionStore);
+        final ProfileManager<CommonProfile> profileManager = new ProfileManager(context);
+        return profileManager.getAll(true);
+    }
 }
--- build.sbt
+++ build.sbt
@@ -11,9 +11,13 @@ scalaVersion := "2.13.0"
 
 libraryDependencies += guice
 libraryDependencies += jdbc
+libraryDependencies += ehcache
 libraryDependencies += "com.h2database" % "h2" % "1.4.199"
 
 // To provide an implementation of JAXB-API, which is required by Ebean.
 libraryDependencies += "javax.xml.bind" % "jaxb-api" % "2.3.1"
 libraryDependencies += "javax.activation" % "activation" % "1.1.1"
 libraryDependencies += "org.glassfish.jaxb" % "jaxb-runtime" % "2.3.2"
+libraryDependencies += "org.pac4j" % "play-pac4j_2.12" % "8.0.0"
+libraryDependencies += "org.pac4j" % "pac4j-oidc" % "3.7.0" exclude("commons-io" , "commons-io")
+libraryDependencies +=  "commons-io" % "commons-io" % "2.4"
